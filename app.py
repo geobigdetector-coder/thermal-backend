d242e35c1d39f753afbd2965175073380a47f062
geobigdetector-coder <geobigdetector@gmail.com>
2025-11-15 23:59:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -207,8 +207,9 @@ def generate():
 def home():
     return "Thermal AI Generator Running!"
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
